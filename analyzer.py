1312210f6c2f206e0d40f2ca855dbe9f9cc6a597
Scott Lackey <scott@clicktripz.com>
2020-02-01 13:25:52 -0500

    addin analyzer

--- analyzer.py
+++ analyzer.py
@@ -1,23 +1,25 @@
 #!/usr/local/bin/python3
 #livongo coding example
-import operator
+import os, operator
 from datetime import datetime, timedelta
+path = "logs/"
 
 d = []
 c = 0
 keys = ['uid','ip', 'date', 'timezone', 'op', 'uri', 'pro', 'statuscode']
-with open("logs/xaa","r") as f:
-  for line in f:
-    #split ws
-    l = line.split()
-    l.remove('-')
-    l.remove('-')
-    uri_string = l[4].split('/')
-    if (len(uri_string) >= 4):
-    #if no uid, don't add it to the dict.
-      uid = uri_string[3]
-      l.insert(0,uid) 
-      d.append(dict(zip(keys, l)))
+for filename in os.listdir(path):
+  with open(os.path.join(path, filename),'r') as f:
+    for line in f:
+      #split ws
+      l = line.split()
+      l.remove('-')
+      l.remove('-')
+      uri_string = l[4].split('/')
+      if (len(uri_string) >= 4):
+      #if no uid, don't add it to the dict.
+        uid = uri_string[3]
+        l.insert(0,uid) 
+        d.append(dict(zip(keys, l)))
 
 def pageviews(d):
   matched = {}
@@ -41,17 +43,29 @@ def pageviews(d):
     i += 1
 
 def sessions(d):
-  sessions = []
+  sess = timedelta(seconds=0)
+  pt = 0
+  session_counter = 0
   for dic in d:
     if (dic['uid'] == '489f3e87'):
       rawdate = dic['date']
-      dt = datetime.strptime(rawdate,"%d/%b/%Y:%H:%M:%S")
-      sessions.append(dt)#.strftime("%d/%b/%Y:%H:%M:%S"))
-      sessions.sort
-    for item in sessions:
-      print(item.strftime("%d/%b/%Y:%H:%M:%S"))
-#      if (item + timedelta(minutes
-#    print (sessions)
+      if (pt == 0):
+        pt = datetime.strptime(rawdate,"%d/%b/%Y:%H:%M:%S")
+        ct = datetime.strptime(rawdate,"%d/%b/%Y:%H:%M:%S")
+      else:
+        pt = ct
+        ct = datetime.strptime(rawdate,"%d/%b/%Y:%H:%M:%S")
+        dt = ct - pt
+#        print(dt.total_seconds())
+        if (dt.total_seconds() <= 600):
+          sess += dt
+        else:
+          print ("############# Session ###############")
+          print(sess.total_seconds())
+          sess = timedelta(seconds=0)
+          session_counter += 1
+  print(sess / timedelta(minutes=1))
+  print ("sessions", session_counter + 1)
 
 pageviews(d)
 sessions(d)
